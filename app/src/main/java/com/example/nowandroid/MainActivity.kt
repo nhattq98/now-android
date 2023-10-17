0d95f64c5d2d4084681ec63ff42ab2d1d6799cd9
Nhật <tahn98@Quangs-MacBook-Pro.local>
2023-10-17 07:34:34 +0700

    first commit

---
+++ app/src/main/java/com/example/nowandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.nowandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+//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
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
