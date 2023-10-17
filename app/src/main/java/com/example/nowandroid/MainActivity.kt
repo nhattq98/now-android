31903f9d4bc49f7c38fb018fcbc2a3bc8e16f071
nhattq <nhattran@vulcanlabs.co>
2023-10-17 15:38:21 +0700

    * migrate to compose

--- app/src/main/java/com/example/nowandroid/MainActivity.kt
+++ app/src/main/java/com/example/nowandroid/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.nowandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
 
 
-//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
+// git access token: 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
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ComposeView>(R.id.compose_view).apply {
+            setContent {
+                HomeScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Surface {
+        Text(text = "Hello Compose")
     }
 }
